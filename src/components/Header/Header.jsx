fd7a87f6e3d46639396bd7899895eecb4a3a4335
Uladzilen Isayenia <Uladzilen.Isayenia@itechart-group.com>
2021-07-02 13:50:08 +0300

    burger menu button added and header styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 import "./Header.scss";
+import logo from "../../resourses/logo.png";
+import { HiOutlineMenu } from "react-icons/hi";
+import { Drawer } from "@material-ui/core";
 
 const Header = () => {
-  return <header className="header"></header>;
+  return (
+    <header className="header">
+      <div className="header-logo-tagline">
+        <img className="header-logo" src={logo} />
+        <h1 className="header-logo-h1">
+          Music<span>Kit</span>
+        </h1>
+      </div>
+      <button className="drawer-button">
+        <HiOutlineMenu className="drawer-icon" />
+      </button>
+      <Drawer></Drawer>
+    </header>
+  );
 };
 
 export default Header;
